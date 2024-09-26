ad40e275c8e11e1bb5054393f185ad1b8dae2c08
aron <arongao123@gmail.com>
2024-09-26 15:02:59 +0800

    init

--- P7_Document/P7_L0_weak_2023_TEST/cpuex_def_h.v
+++
@@ -1,5 +0,0 @@
-`define L_adEx 5'h4
-`define S_adEx 5'h5
-`define Syscall 5'h8
-`define RI 5'ha
-`define Ov 5'hc
--- P7_Document/P7_L0_weak_2023_TEST/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/Aron_/Desktop/lessons/SecondYear1/CO/Verilog_Project/P7_L0_weak_2023_TEST/mips_txt_isim_beh.exe" -prj "C:/Users/Aron_/Desktop/lessons/SecondYear1/CO/Verilog_Project/P7_L0_weak_2023_TEST/mips_txt_beh.prj" "work.mips_txt" "work.glbl" 
